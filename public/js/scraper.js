aa73b8eed78beb0327abfe1334f4ff6e60d687ef
Danny Trépanier <danny-trepanier@hotmail.com>
2021-11-18 18:08:46 -0500

    Modif du scraper. Il y a plus d'espace dans le nom de la bouteille.

--- public/js/scraper.js
+++ public/js/scraper.js
@@ -1,28 +1,37 @@
 
+const { result } = require('lodash');
 const puppeteer = require('puppeteer');
 
 
 // https://www.youtube.com/watch?v=TzZ3YOUhCxo  Youtube video sur comment utiliser Puppeteer
-async function scrapeProduct(url) {
+async function scrapeProduct(numberPage, product_list_limit) {
 
-	const browser = await puppeteer.launch();
-	const page  = await browser.newPage();
-	await page.goto(url);
-	for(let i=1; i<=96; i++) {
-		const [el] = await page.$x('/html/body/div[3]/div[2]/div[1]/main/div/div[2]/div[3]/ol/li['+ i +']/div/div[3]/div[1]/div[1]/strong[1]/a');
-		const src = await el.getProperty('textContent');
-		const srcText = await src.jsonValue();
+    for(let i=1; i<=numberPage; i++) {
+        url = 'https://www.saq.com/fr/produits/vin?p='+ i +'&product_list_limit='+ product_list_limit +'';
 
-		console.log(srcText.replace(/^\s+|\s+$/gm,''));
-	}
-}
-
-// On fait la boucle pour appeler toutes les pages des produits vins de la SAQ
-async function sendRequest() {
-
-    for(let i=1; i<=1; i++) {
-        scrapeProduct('https://www.saq.com/fr/produits/vin?p='+ i +'&product_list_limit=96');
+        const browser = await puppeteer.launch();
+        const page  = await browser.newPage();
+        await page.goto(url);
+        for(let j=1; j<=product_list_limit; j++) {
+            try {
+                const [el] = await page.$x('/html/body/div[3]/div[2]/div[1]/main/div/div[2]/div[3]/ol/li['+ j +']/div/div[3]/div[1]/div[1]/strong[1]/a');
+                const src = await el.getProperty('textContent');
+                const srcText = await src.jsonValue();
+                console.log('Page: ' + i + ' id=' + j + ' => ' + srcText.replace(/^\s+|\s+$/g, "").replace(/\s+/g, " "));
+            }
+            catch (error) {
+                console.log("Impossible de prendre la bouteille " +j);
+            }
+        }
     }
 }
 
-sendRequest();
+// On fait la boucle pour appeler le nombre de page et le nombre de produit limite par page demandé par l'admin
+// function sendRequest(numberPage, product_list_limit) {
+
+//     for(let i=1; i<=numberPage; i++) {
+//         scrapeProduct('https://www.saq.com/fr/produits/vin?p='+ i +'&product_list_limit='+ product_list_limit +'', i, product_list_limit);
+//     }
+// }
+
+scrapeProduct(5,96);
