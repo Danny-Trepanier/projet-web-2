2dfd23831ad8ee32750e0aeec7c234faafa7425c
Maxime Fournier <studio.rouge@icloud.com>
2021-11-23 14:38:26 -0500

    correction du component de jetstream du à une manipulation à deux personnes dans les dossiers

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,17 +1,8 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-<<<<<<< HEAD
-    <div>
-        {{ $logo }}
-    </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-=======
-
     <div class="logo">
         {{ $logo }}
     </div>
     <div class="formulaire">
->>>>>>> a98ffb0db8ccdbb8e7d38d96e15fdae288908140
         {{ $slot }}
     </div>
 </div>
