3e9ba3aab753eee5d0eafb84ace796aa7af9e8c7
Maxime Fournier <studio.rouge@icloud.com>
2021-11-22 21:15:34 -0500

    modification des logo svg en png

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,52 +1,52 @@
 <x-guest-layout>
 
-        <img src="/storage/logo-vino-noir.svg" alt="Logo du site Vino en noir" class="logo-noir">
-        <h1>Vino</h1>
+    <img src="{{ asset('img/logo-vino-noir.png') }}" alt="Logo du site Vino en noir" class="logo-noir">
+    <h1>Vino</h1>
 
-        <x-jet-validation-errors class="error"/>
+    @if (session('status'))
+        <div class="status">
+            {{ session('status') }}
+        </div>
+    @endif
 
-        @if (session('status'))
-            <div class="status">
-                {{ session('status') }}
-            </div>
-        @endif
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div class="connexion--login-container">
-                <h2>Log in</h2>
-                <div>
-                    <x-jet-label for="email" value="{{ __('Email') }}"/>
-                    <x-jet-input id="email" type="email" name="email" :value="old('email')" required autofocus/>
-                </div>
-                <div>
-                    <x-jet-label for="password" value="{{ __('Password') }}" />
-                    <x-jet-input id="password" type="password" name="password" required autocomplete="current-password"/>
-                </div>
-                <div>
-                    <label for="remember_me">
-                        <x-jet-checkbox id="remember_me" name="remember"/>
-                        <span>{{ __('Remember me') }}</span>
-                    </label>
-                </div>
-            </div>
-
+        <div class="connexion--login-container">
+            <h2>Log in</h2>
+            <x-jet-validation-errors id="email" class="error"/>
             <div>
-                @if (Route::has('password.request'))
-                    <a href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="button">
-                    {{ __('Log in') }}
-                </x-jet-button>
-                <span>or register : </span>
-                <x-jet-button class="button">
-                    <a href="{{ route('register') }}">Register</a>
-                </x-jet-button>
+                <x-jet-label for="email" value="{{ __('Email') }}"/>
+                <x-jet-input id="email" type="email" name="email" :value="old('email')"/>
             </div>
-        </form>
+            <div>
+                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-input id="password" type="password" name="password"/>
+            </div>
+            <div>
+                <label for="remember_me">
+                    <x-jet-checkbox id="remember_me" name="remember"/>
+                    <span>{{ __('Remember me') }}</span>
+                </label>
+            </div>
+        </div>
+
+        <div>
+            @if (Route::has('password.request'))
+                <a href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-jet-button class="button">
+                {{ __('Log in') }}
+            </x-jet-button>
+
+            <span>or register : </span>
+            <x-jet-button class="button">
+                <a href="{{ route('register') }}">Register</a>
+            </x-jet-button>
+        </div>
+    </form>
 
 </x-guest-layout>
