d8d246d06dd9848e070b6d4564d73bf873cf50a5
Danny Trépanier <danny-trepanier@hotmail.com>
2021-11-24 17:17:53 -0500

    Ajout du fichier messages.php pour les textes en anglais de la vue Connexion

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Messages Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used for various texts that we need to
+| display to the user.
+|
+*/
+
+return [
+
+    // Login page
+    'login_title_page'   => 'My account',
+    'login_form_title'   => 'Login',
+    'login_required_fields_text'   => '* Required fields',
+    'login_label_for_email'   => 'Email',
+    'login_label_for_password'   => 'Password',
+    'login_button_text'   => 'Login',
+    'login_link_forgot_password'   => 'Forgot your password ?',
+];
