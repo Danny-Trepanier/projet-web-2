374257c2df3d1c3741932d5cb75d53a4d572b71e
Maxime Fournier <studio.rouge@icloud.com>
2021-11-25 13:49:27 -0500

    correction de la route apres le login et integration graphique de create.css cellar avec le html de create.blade.php de cellar

--- resources/views/cellar/create.blade.php
+++ resources/views/cellar/create.blade.php
@@ -1,23 +1,20 @@
 <x-app-layout>
+
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <h1>Create Cellar</h1>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <h1>Création d'un cellier</h1>
+    <div class="cellar--create">
+                <h2>Creation of a cellar</h2>
                 <form action="" method="POST">
                     @csrf
                     <fieldset>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1" class="form-label mt-4">Nom du cellier</label>
+                        <div>
+                            <label for="exampleInputEmail1">Cellar name</label>
                             <input type="text" class="form-control" name="name">
                         </div>
 
-                        <div class="form-floating mt-3">
+                        <div>
                             @if ($errors->any())
                                 <div class="alert alert-danger">
                                     <ul>
@@ -29,12 +26,11 @@
                             @endif
                         </div>
 
-                        <div class="mt-3 mb-3">
-                            <button type="submit" class="btn btn-primary">Créer</button>
+                        <div>
+                            <button type="submit" class="button">Create</button>
                         </div>
                     </fieldset>
                 </form>
-            </div>
-        </div>
     </div>
+
 </x-app-layout>
