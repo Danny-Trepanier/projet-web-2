254365280b481e83f015d61ff0d48219e5ec5e54
Jean-Philippe Desloges <jp.desloges@gmail.com>
2021-11-25 23:29:39 -0500

    ajout d'icones et modif d'icones, continuation de la vue index bouteille et de son css

--- resources/views/bottle/index.blade.php
+++ resources/views/bottle/index.blade.php
@@ -5,43 +5,69 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                @forelse ($bottles as $bottle)
+	<section class="filters">
+		<div class="filters--wrap">
+			<div class="filters--button"></div>
+			<div class="filters--button"></div>
+			<div class="filters--button"></div>
+			<div class="filters--button"></div>
+			<div class="filters--button"></div>
+		</div>
+		<div class="filters--search-bar">
+			<input type="text" placeholder="Nom, Pays, Couleur, Prix... ">
+		</div>
+	</section>
 
-                <a href="{{ url("") }}/bottle/{{ $bottle->id }}">
-					<article class="wine-card">
-						<div class="img-wrap">
-							<img src="{{ $bottle->image_link }}?quality=80&fit=bounds&height=166&width=111&canvas=111:166 " alt="{{ $bottle->name }}">
-						</div>
-						<div class="info-wrap">
-							<div class="info--text">
-								<h1>{{ $bottle->name }}</h1>
-								<p>{{ $bottle->country }}</p>
-								<p>{{ $bottle->price }}$</p>
-							</div>
+@forelse ($bottles as $bottle)
+	<a href="{{ url("") }}/bottle/{{ $bottle->id }}">
+		<article class="wine-card">
+			<div class="img-wrap">
+				<img src="{{ $bottle->image_link }}?quality=80&fit=bounds&height=166&width=111&canvas=111:166 " alt="{{ $bottle->name }}">
+			</div>
+			<div class="info-wrap">
+				<div class="info--text">
+					<h1>{{ $bottle->name }}</h1>
+					<div>
+						<p>{{ $bottle->country }}</p>
+						<p>{{ $bottle->price }}$</p>
+					</div>
+				</div>
+				<div class="info--icons">
+					
+<!-- Déploiement de l'icone de couleur selon les infos de la bouteille  -->
+		@if($bottle->color == 'rouge')
+					<img src="{{ asset('img/icon/icone_vin_rouge.png') }}" alt="icone vin rouge">
+		@elseif($bottle->color == 'blanc')
+					<img src="{{ asset('img/icon/icone_vin_blanc.png') }}" alt="icone vin rouge">
+		@elseif($bottle->color == 'rosé')
+					<img src="{{ asset('img/icon/icone_vin_rose.png') }}" alt="icone vin rouge">
+		@endif
 
-
-							<div class="info--icons">
-
-							@forelse($comments as $comment)
+<!-- Affichage de la note laissée par l'usager sur la bouteille -->
+		@forelse($comments as $comment)
 			@if($bottle->id == $comment->bottle_id)
-							<p>{{ $comment->note }}</p>
-				@else <p>0</p>
-			@endif
-			@empty
-							<p>0</p>
-		@endforelse
-							</div>
-						</div>
-					</article>
-				</a>
+					<div>
+						<span>{{ $comment->note }}</span> 
+						<img src="{{ asset('img/icon/icon_etoile_rouge.png') }}" alt="icone etoile note">
 
-                @empty
-                <p>Il y a aucune bouteille dans la base de donnée.</p>
-                @endforelse
-            </div>
-        </div>
-    </div>
+					</div>
+
+		@else 
+					<p>/
+					</p>
+			@endif
+		@empty
+					<p>/
+					</p>
+		@endforelse
+
+				</div>
+			</div>
+		</article>
+	</a>
+
+	@empty
+	<p>Il y a aucune bouteille dans la base de donnée.</p>
+	@endforelse
+
 </x-app-layout>
