e90b3017090fd6e051f5e2865024d1b165b280c6
Jean-Philippe Desloges <jp.desloges@gmail.com>
2021-11-26 09:59:34 -0500

    ajout de la barre de recherche dans la view et le css

--- resources/views/bottle/index.blade.php
+++ resources/views/bottle/index.blade.php
@@ -7,11 +7,24 @@
 
 	<section class="filters">
 		<div class="filters--wrap">
-			<div class="filters--button"></div>
-			<div class="filters--button"></div>
-			<div class="filters--button"></div>
-			<div class="filters--button"></div>
-			<div class="filters--button"></div>
+			<div class="filters--button">
+				<span>A-Z</span>
+			</div>
+			<div class="filters--button">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 30" x="0px" y="0px"><g data-name="Layer 2"><path d="M5,14.55a12.15,12.15,0,0,0,7.24.42,10.14,10.14,0,0,1,7,.77l1.76.88V4.38l-.86-.43A12.16,12.16,0,0,0,11.76,3a10.15,10.15,0,0,1-7-.77L3,1.38V22H5Zm0-10A12.15,12.15,0,0,0,12.24,5,10.14,10.14,0,0,1,19,5.62v7.83A12.15,12.15,0,0,0,11.76,13,10.16,10.16,0,0,1,5,12.38Z"/></g></svg>
+			</div>
+			<div class="filters--button">
+
+			</div>
+			<div class="filters--button">
+			<span>$</span>
+			</div>
+			<div class="filters--button">
+				<svg xmlns="http://www.w3.org/2000/svg" width="33" height="31" viewBox="0 0 51 48">
+				<title>Five Pointed Star</title>
+				<path fill="none" stroke="#000" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
+				</svg>
+			</div>
 		</div>
 		<div class="filters--search-bar">
 			<input type="text" placeholder="Nom, Pays, Couleur, Prix... ">
