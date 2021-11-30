689b8d93d6400e6238e5e5d7ade2430947831960
Maxime Fournier <studio.rouge@icloud.com>
2021-11-30 09:37:06 -0500

    Ajout des celliers de l'utilisateur dans le controller BottleController.php(show)

--- app/Http/Controllers/BottleController.php
+++ app/Http/Controllers/BottleController.php
@@ -101,8 +101,12 @@ class BottleController extends Controller
      */
     public function show(Bottle $bottlePost)
     {
+        // Nous prenons tous les celliers de l'utilisateur
+        $myCellars = Cellar::get()->where('user_id', Auth::user()->id);
+
         return view('bottle.show', [
             'bottle' => $bottlePost,
+            'myCellars' => $myCellars,
         ]);
     }
 
