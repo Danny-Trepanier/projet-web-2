5016ff7c2d4c114e8bce45157f1113e90e1ce0f5
Maxime Fournier <studio.rouge@icloud.com>
2021-12-01 14:16:40 -0500

    HTML de le view show.blade.php et CellarController.php pour ajouter la fonction de count de bouteilles

--- resources/views/cellar/show.blade.php
+++ resources/views/cellar/show.blade.php
@@ -31,16 +31,71 @@
 	</section>
 
     <div>
-        <p>Nom du user: {{ $cellar->user->name }}</p>
 
-        <p>Dans mon cellier, il y a:</p>
-        <ul>
-            @forelse ($myCellars->bottles as $myBottle)
-                <li>{{ $myBottle->name }}</li>
-            @empty
-                <p>Vous possèdez aucune bouteille dans ce cellier.</p>
-            @endforelse
-        </ul>
+        {{-- <p>Nom du user: {{ $cellar->user->name }}</p> --}}
+        {{-- <p>Dans mon cellier, il y a: {{ count($myCellars->bottles) }}</p> --}}
+
+        @forelse($myCellars->bottles as $myBottle)
+
+        {{-- {{ dd($quantites) }} --}}
+
+            {{-- @forelse($quantites as $quantite) --}}
+
+                {{-- @if($quantite->bottle_id = $myBottle->id) --}}
+                    {{-- {{ $count++ }} --}}
+                {{-- @endif --}}
+
+            {{-- @endforelse --}}
+
+            <a href="{{ url("") }}/bottle/{{ $myBottle->id }}">
+                <article class="wine-card">
+                    <div class="img-wrap">
+                        <img src="{{ $myBottle->image_link }}?quality=80&fit=bounds&height=166&width=111&canvas=111:166 " alt="{{ $myBottle->name }}">
+                    </div>
+                    <div class="info-wrap">
+                        <div class="info--text">
+                            <h1>{{ $myBottle->name }}</h1>
+                            <div>
+                                <p>{{ $myBottle->country }}</p>
+                                <p>Qunatité(s) : {{ $count }}</p>
+                                <p>{{ $myBottle->price }}$</p>
+                            </div>
+                        </div>
+                        <div class="info--icons">
+
+        <!-- Déploiement de l'icone de couleur selon les infos de la bouteille  -->
+                @if($myBottle->color == 'rouge')
+                            <img src="{{ asset('img/icon/icone_vin_rouge.png') }}" alt="icone vin rouge">
+                @elseif($myBottle->color == 'blanc')
+                            <img src="{{ asset('img/icon/icone_vin_blanc.png') }}" alt="icone vin rouge">
+                @elseif($myBottle->color == 'rosé')
+                            <img src="{{ asset('img/icon/icone_vin_rose.png') }}" alt="icone vin rouge">
+                @endif
+
+        <!-- Affichage de la note laissée par l'usager sur la bouteille -->
+                @forelse($comments as $comment)
+                    @if($myBottle->id == $comment->bottle_id)
+                        <div>
+                            <span><b>{{ $comment->note }}</b></span>
+                            <img src="{{ asset('img/icon/icon_etoile_rouge.png') }}" alt="icone etoile note">
+                        </div>
+                    @endif
+                @empty
+                    <div>
+                        <span><b>/</b></span>
+                        <img src="{{ asset('img/icon/icon_etoile_vide.png') }}" alt="icone etoile vide">
+                    </div>
+                @endforelse
+
+                        </div>
+                    </div>
+                </article>
+            </a>
+
+        @empty
+            <p>Il y a aucune bouteille dans la base de donnée.</p>
+        @endforelse
+
     </div>
 
 </x-app-layout>
