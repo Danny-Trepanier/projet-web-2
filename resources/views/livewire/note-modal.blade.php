f59a941aadc9e686bdcdfc790bae08beb7bcc177
Jean-Philippe Desloges <jp.desloges@gmail.com>
2021-12-02 14:30:09 -0500

    ajout de l automatisation des etoiles de note

--- resources/views/livewire/note-modal.blade.php
+++ resources/views/livewire/note-modal.blade.php
@@ -1,7 +1,14 @@
 <div class="modal--wrap" >
-	<span>{{ $comment[0]->note ?? '/' }}</span>
+	<span class="note--number">{{ $comment[0]->note ?? '/' }}</span>
 
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 225"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M120,0l30,85h90l-70,55,25,85-75-50L45,225l25-85L0,85H90Z"/></g></g></svg>
+@if(isset($comment[0]->note))
+	@for ($i = 0; $i < $comment[0]->note; $i++)
+		<svg class="icon--star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 225"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M120,0l30,85h90l-70,55,25,85-75-50L45,225l25-85L0,85H90Z"/></g></g></svg>
+	@endfor
+
+@else
+	<svg class="icon--star-empty" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 225"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M120,0l30,85h90l-70,55,25,85-75-50L45,225l25-85L0,85H90Z"/></g></g></svg>
+@endif
 
 	<form class="modal-form form--hidden" action="{{ route('bottle.create.comment') }}" method="POST">
 	@csrf
