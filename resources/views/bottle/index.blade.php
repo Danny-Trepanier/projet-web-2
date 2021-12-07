79c628ab905444d20bd21f441ae77955243ddfd2
Jean-Philippe Desloges <jp.desloges@gmail.com>
2021-12-07 11:37:17 -0500

    changement du titre de la page index de Bottle et traduction

--- resources/views/bottle/index.blade.php
+++ resources/views/bottle/index.blade.php
@@ -1,9 +1,8 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+
+	<div class="title">
+        <h1>{{ __('messages.bottle_index_title') }}</h1>
+    </div>
 
 	<section class="filters">
 		<div class="filters--wrap">
@@ -30,62 +29,63 @@
 			<input type="search" placeholder=" Nom, Pays, Couleur, Prix... " results="0">
 		</div>
 	</section>
-
-@forelse ($bottles as $bottle)
-	<a href="{{ url("") }}/bottle/{{ $bottle->id }}">
-		<article class="wine-card">
-			<div class="img-wrap">
-				@if(filter_var($bottle->image_link, FILTER_VALIDATE_URL))
-				<img src="{{ $bottle->image_link }}?quality=80&fit=bounds&height=166&width=111&canvas=111:166 " alt="{{ $bottle->name }}">
-				@else
-				<img src="{{ asset('storage/bouteille-default.png') }} " alt="{{ $bottle->name }}">
-				@endif
-			</div>
-			<div class="info-wrap">
-				<div class="info--text">
-					<h1>{{ $bottle->name }}</h1>
-					<div>
-						<p>{{ $bottle->country }}</p>
-						<p>{{ $bottle->price }}$</p>
+	<section class="liste-wrap">
+		@forelse ($bottles as $bottle)
+			<a href="{{ url("") }}/bottle/{{ $bottle->id }}">
+				<article class="wine-card">
+					<div class="img-wrap">
+						@if(filter_var($bottle->image_link, FILTER_VALIDATE_URL))
+						<img src="{{ $bottle->image_link }}?quality=80&fit=bounds&height=166&width=111&canvas=111:166 " alt="{{ $bottle->name }}">
+						@else
+						<img src="{{ asset('storage/bouteille-default.png') }} " alt="{{ $bottle->name }}">
+						@endif
 					</div>
-				</div>
-
-				<div class="info--icons">
-					
-<!-- Déploiement de l'icone de couleur selon les infos de la bouteille  -->
-					@if($bottle->color == 'rouge')
-					<img src="{{ asset('img/icon/icone_vin_rouge.png') }}" alt="icone vin rouge">
-					@elseif($bottle->color == 'blanc')
-					<img src="{{ asset('img/icon/icone_vin_blanc.png') }}" alt="icone vin rouge">
-					@elseif($bottle->color == 'rosé')
-					<img src="{{ asset('img/icon/icone_vin_rose.png') }}" alt="icone vin rouge">
-					@endif
-
-<!-- Affichage de la note laissée par l'usager sur la bouteille -->
-					@forelse($comments as $comment)
-						@if($bottle->id == $comment->bottle_id)
-					<div>
-						<span><b>{{ $comment->note }}</b></span> 
-						<img src="{{ asset('img/icon/icon_etoile_rouge.png') }}" alt="icone etoile note">
+					<div class="info-wrap">
+						<div class="info--text">
+							<h1>{{ $bottle->name }}</h1>
+							<div>
+								<p>{{ $bottle->country }}</p>
+								<p>{{ $bottle->price }}$</p>
+							</div>
+						</div>
+		
+						<div class="info--icons">
+		
+		<!-- Déploiement de l'icone de couleur selon les infos de la bouteille  -->
+							@if($bottle->color == 'rouge')
+							<img src="{{ asset('img/icon/icone_vin_rouge.png') }}" alt="icone vin rouge">
+							@elseif($bottle->color == 'blanc')
+							<img src="{{ asset('img/icon/icone_vin_blanc.png') }}" alt="icone vin rouge">
+							@elseif($bottle->color == 'rosé')
+							<img src="{{ asset('img/icon/icone_vin_rose.png') }}" alt="icone vin rouge">
+							@endif
+		
+		<!-- Affichage de la note laissée par l'usager sur la bouteille -->
+							@forelse($comments as $comment)
+								@if($bottle->id == $comment->bottle_id)
+							<div>
+								<span><b>{{ $comment->note }}</b></span>
+								<img src="{{ asset('img/icon/icon_etoile_rouge.png') }}" alt="icone etoile note">
+							</div>
+		
+							@else
+		
+							@endif
+						@empty
+							<div>
+								<span><b>/</b></span>
+								<img src="{{ asset('img/icon/icon_etoile_vide.png') }}" alt="icone etoile vide">
+							</div>
+				@endforelse
+		
+						</div>
 					</div>
-
-					@else 
-
-					@endif
-				@empty
-					<div>
-						<span><b>/</b></span> 
-						<img src="{{ asset('img/icon/icon_etoile_vide.png') }}" alt="icone etoile vide">
-					</div>
-		@endforelse
-
-				</div>
-			</div>
-		</article>
-	</a>
-
-	@empty
-	<p>Il y a aucune bouteille dans la base de donnée.</p>
-	@endforelse
+				</article>
+			</a>
+		
+			@empty
+			<p>Il y a aucune bouteille dans la base de donnée.</p>
+			@endforelse
+	</section>
 
 </x-app-layout>
