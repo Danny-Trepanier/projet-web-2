8c0d33141bb1fcdb0fc73f6ed8acb4c9685c3eb4
Jean-Philippe Desloges <jp.desloges@gmail.com>
2021-12-08 10:23:09 -0500

    restore de la modale qui fonctionne

--- app/Http/Livewire/ButtonAddBottle.php
+++ app/Http/Livewire/ButtonAddBottle.php
@@ -8,18 +8,31 @@ class ButtonAddBottle extends Component
 {
 
 	public $quantity = 0;
+	public $show;
+
+	public function mount() 
+	{
+
+	}
 
     public function render()
     {
         return view('livewire.button-add-bottle');
     }
 
-	public function add() {
+	public function add() 
+	{
 		
 		$this->quantity++;
 	}
 
-	public function substract() {
+	public function substract() 
+	{
 
+	}
+
+	public function showModal() 
+	{
+		
 	}
 }
