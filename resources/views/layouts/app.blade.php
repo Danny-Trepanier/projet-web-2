00581e005c63dc74b0aa793f4bf9ea7c50b46322
Jean-Philippe Desloges <jp.desloges@gmail.com>
2021-12-08 15:52:33 -0500

    enlever Alpine.JS dans le layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
-		<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
+
     </head>
     <body>
 
