9ca3c7af0160e02dea271365aee9430b1b6409a4
Maxime Fournier <studio.rouge@icloud.com>
2021-12-09 10:10:35 -0500

    Ajout de le barre de recherche à corriger

--- app/Service/ResearchText.php
+++ app/Service/ResearchText.php
@@ -3,19 +3,47 @@
 namespace App\Service;
 
 use App\Models\Bottle;
-use App\Models\BottleCellar;
 use App\Models\Cellar;
+use Illuminate\Support\Str;
+use App\Models\BottleCellar;
 
 // Tutoriel youtube https://www.youtube.com/watch?v=QfZgqYV5pac
 
 class ResearchText {
 
-    public static function searchByText($myCellars, string $text)
+    public $myCellars;
+
+    public static function searchByName(string $name, $myCellars): array
     {
-        return collect($myCellars)->filter(function($value, $key) {
-            dump($value, $key);
-        });
+        return collect($myCellars)
+            ->filter(fn($object) => Str::contains(strtolower($object->name), strtolower($name)))
+            ->all();
     }
+
+    // public static function searchByCountry($myCellars, string $country): array
+    // {
+    //     return collect($myCellars)
+    //         ->filter(fn($object) => Str::contains(strtolower($object->country), strtolower($country)))
+    //         ->all();
+    // }
+
+    // public static function searchByColor($myCellars, string $color): array
+    // {
+    //     return collect($myCellars)
+    //         ->filter(fn($object) => Str::contains(strtolower($object->color), strtolower($color)))
+    //         ->all();
+    // }
+
+    // public static function searchByPrice($myCellars, string $price): array
+    // {
+    //     return collect($myCellars)
+    //         ->filter(fn($object) => Str::contains(strtolower($object->price), strtolower($price)))
+    //         ->all();
+    // }
+    // public static function arrayCellar()
+    // {
+    //     return $this->myCellars;
+    // }
 
 }
 
--- resources/views/livewire/research.blade.php
+++ resources/views/livewire/research.blade.php
@@ -1,6 +1,63 @@
-<div class="filters--search-bar">
+<div>
+    <div class="filters--search-bar">
+        <input wire:model.debounce.1s="name" type="text">
+    </div>
 
-    <input wire:model.debounce.1s="text" type="search" placeholder=" Nom, Pays, Couleur, Prix... " results="0">
+    <div>
+    {{-- {{ dd($name) }} --}}
+        {{-- @if (count($allBottles) = 0) --}}
+        @forelse($allBottles as $myBottle)
 
+            <a href="{{ url("") }}/bottle/{{ $myBottle->id }}">
+                <article class="wine-card">
+                    <div class="img-wrap">
+                        <img src="{{ $myBottle->image_link }}?quality=80&fit=bounds&height=166&width=111&canvas=111:166 " alt="{{ $myBottle->name }}">
+                    </div>
+                    <div class="info-wrap">
+                        <div class="info--text">
+                            <h1>{{ $myBottle->name }}</h1>
+                            <div>
+                                <p>{{ $myBottle->country }}</p>
+                                <p>Quantité(s) :
+                                    {{ $myBottle->bottleCount }}
+                                </p>
+                                <p>{{ $myBottle->price }}$</p>
+                            </div>
+                        </div>
+                        <div class="info--icons">
+
+        <!-- Déploiement de l'icone de couleur selon les infos de la bouteille  -->
+                @if($myBottle->color == 'rouge')
+                            <img src="{{ asset('img/icon/icone_vin_rouge.png') }}" alt="icone vin rouge">
+                @elseif($myBottle->color == 'blanc')
+                            <img src="{{ asset('img/icon/icone_vin_blanc.png') }}" alt="icone vin rouge">
+                @elseif($myBottle->color == 'rosé')
+                            <img src="{{ asset('img/icon/icone_vin_rose.png') }}" alt="icone vin rouge">
+                @endif
+
+        <!-- Affichage de la note laissée par l'usager sur la bouteille -->
+                            {{-- @if($myBottle->comment)
+                                <div>
+                                    <span><b>{{ $myBottle->comment->note }}</b></span>
+                                    <img src="{{ asset('img/icon/icon_etoile_rouge.png') }}" alt="icone etoile note">
+                                </div>
+                            @else
+                                <div>
+                                    <span><b>/</b></span>
+                                    <img src="{{ asset('img/icon/icon_etoile_vide.png') }}" alt="icone etoile vide">
+                                </div>
+                            @endif --}}
+                        </div>
+                    </div>
+                </article>
+            </a>
+
+            @empty
+            <p>Il y a aucune bouteille dans la base de donnée.</p>
+            @endforelse
+            {{-- @endif --}}
+
+    </div>
 </div>
+
 
