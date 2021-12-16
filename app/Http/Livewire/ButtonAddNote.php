107162cfc246e48f70ebaab95e473fd9c4827211
Jean-Philippe Desloges <jp.desloges@gmail.com>
2021-12-15 22:35:44 -0500

    modif du component note-modal et button-add-note pour changer la note sans faire recharger la page

--- app/Http/Livewire/ButtonAddNote.php
+++ app/Http/Livewire/ButtonAddNote.php
@@ -3,10 +3,15 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class ButtonAddNote extends Component
 {
 	public $bottle;
+	public $note;
+
+	protected $listeners = ['noteChange' => 'getNote'];
 
 	public function mount($bottle)
 	{
@@ -18,8 +23,22 @@ class ButtonAddNote extends Component
 		$this->emit(event: 'showNoteModal');
 	}
 
+	public function getNote()
+	{
+
+		$currentNote = DB::table('comments')
+						->select('note')
+						->where('bottle_id', '=', $this->bottle->id)
+						->where('user_id', '=', Auth::user()->id)
+						->get();
+		$currentNote = (int) filter_var($currentNote, FILTER_SANITIZE_NUMBER_INT);
+		$this->note = $currentNote;
+		return $this->note;
+	}
+
     public function render()
     {
+		$this->getNote();
         return view('livewire.button-add-note');
     }
 }
