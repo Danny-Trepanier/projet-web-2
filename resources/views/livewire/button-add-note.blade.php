63347f090246b2d350b8d448a6e0a961b3a27cda
Jean-Philippe Desloges <jp.desloges@gmail.com>
2021-12-16 15:35:05 -0500

    optimization du component de la note modale pour ne plus utiliser le formulaire

--- resources/views/livewire/button-add-note.blade.php
+++ resources/views/livewire/button-add-note.blade.php
@@ -2,13 +2,14 @@
 	<button class="note--number" wire:click="showNoteModal">
 		<span>{{ $note ?? '/' }}</span>
 	
-@if($bottle->comment)
-	@for ($i = 0; $i < $bottle->comment->note; $i++)
-		<svg class="icon--star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 225"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M120,0l30,85h90l-70,55,25,85-75-50L45,225l25-85L0,85H90Z"/></g></g></svg>
+@if($note)
+	@for ($i = 0; $i < $note; $i++)
+
+	<img class="icon--star" src="{{asset('img/icon/icon_etoile_rouge.png')}}" alt="red star note">
 	@endfor
 
 	@else
-		<svg class="icon--star-empty" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 225"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M120,0l30,85h90l-70,55,25,85-75-50L45,225l25-85L0,85H90Z"/></g></g></svg>
+	<img class="icon--star" src="{{asset('img/icon/icon_etoile_vide.png')}}" alt="empty star note">
 	</button>
 @endif
 </div>
--- resources/views/livewire/note-modal.blade.php
+++ resources/views/livewire/note-modal.blade.php
@@ -1,37 +1,33 @@
 @if($showNote == true)
 <div class="modal--fullscreen">
 	<div class="modal-note-content">
-		<form>
-		@csrf
 			<fieldset class="wrap--btn-note">
-					<input name="bottle_id" type="hidden" value="{{ $bottle->id }}">
+				<input name="bottle_id" type="hidden" value="{{ $bottle->id }}">
 
-					<div>
-						<label for="note">1</label>
-						<input wire:model="note" type="radio" name="note" value="1">
-					</div>
-					<div>
-						<label for="note">2</label>
-						<input wire:model="note" type="radio" name="note" value="2">	
-					</div>
-					<div>
-						<label for="note">3</label>
-						<input wire:model="note" type="radio" name="note" value="3">
-					</div>
-					<div>
-						<label for="note">4</label>
-						<input wire:model="note" type="radio" name="note" value="4">
-					</div>
-					<div>
-						<label for="note">5</label>
-						<input wire:model="note" type="radio" name="note" value="5">
-					</div>
-		
-					<button wire:click.prevent="storeComment( {{$bottle->id}}, {{ $note }})">Note</button>
-					<button wire:click.prevent='closeNoteModal'>X</button>
-
+				<div>
+					<label for="note">1</label>
+					<input wire:model="note" type="radio" name="note" value="1">
+				</div>
+				<div>
+					<label for="note">2</label>
+					<input wire:model="note" type="radio" name="note" value="2">	
+				</div>
+				<div>
+					<label for="note">3</label>
+					<input wire:model="note" type="radio" name="note" value="3">
+				</div>
+				<div>
+					<label for="note">4</label>
+					<input wire:model="note" type="radio" name="note" value="4">
+				</div>
+				<div>
+					<label for="note">5</label>
+					<input wire:model="note" type="radio" name="note" value="5">
+				</div>
+	
+				<button wire:click.prevent="storeComment( {{$bottle->id}}, {{ $note }})">Note</button>
+				<button wire:click.prevent='closeNoteModal'>X</button>
 			</fieldset>
-		</form>
 	</div>
 </div>
 @else
