b078c361488eb59d6cda5695e63f599f4ae88667
Maxime Fournier <studio.rouge@icloud.com>
2021-12-16 14:56:05 -0500

    Changement du message quand le nom de bouteille dans la liste de la SAQ est introuvable

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -118,6 +118,7 @@ return [
     'bottle_index_search_bar_placeholder'   => 'Name, Country, Color, Price...',
     'bottle_index_database_empty'   => 'There are no bottles in the database.',
     'bottle_index_empty'   => 'Search for a bottle by name.',
+    'bottle_index_search_result_empty'   => 'There is no bottle of his name in the list.',
 
     //Page show.blade.php de Bottle
 	'bottle_show_title'   => 'Bottle details',
