4324871552f880bc9cdd43bd7010b39d6728a956
Danny Trépanier <danny-trepanier@hotmail.com>
2021-12-19 21:19:43 -0500

    Ajout d'un lien pour changer la langue dans le menu du panel admin

--- resources/views/vendor/nova/partials/user.blade.php
+++ resources/views/vendor/nova/partials/user.blade.php
@@ -13,6 +13,12 @@
 
 <dropdown-menu slot="menu" width="200" direction="rtl">
     <ul class="list-reset">
+        @php $locale = session()->get('locale'); @endphp
+        @if($locale == 'fr')
+            <li><a href="{{ url("") }}/lang/en" class="block no-underline text-90 hover:bg-30 p-3">{{ __('messages.menu_link_english') }}</a></li>
+        @else
+            <li><a href="{{ url("") }}/lang/fr" class="block no-underline text-90 hover:bg-30 p-3">{{ __('messages.menu_link_french') }}</a></li>
+        @endif
         <li>
             <a href="{{ route('nova.logout') }}" class="block no-underline text-90 hover:bg-30 p-3">
                 {{ __('Logout') }}
